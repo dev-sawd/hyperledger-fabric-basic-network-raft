07d58bb9f187e0ec953ad0ac2d6dee61a394569e
devsawd <devsawd@gmail.com>
2020-06-14 19:54:07 +0900

    add bin folder

--- start.sh
+++ start.sh
@@ -10,7 +10,7 @@ rm -rf ./config
 mkdir config
 rm -rf crypto-config
 
-export PATH=$HOME/fabric-samples/bin:$PATH
+export PATH=./bin:$PATH
 SYS_CHANNEL="byfn-sys-channel"
 
 cryptogen generate --config=./crypto-config.yaml
